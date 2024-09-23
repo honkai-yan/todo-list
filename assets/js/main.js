49cd9c1a72a943b894bc4f8b988676ce1ca9f8d2
honkai-yan <2718504358@qq.com>
2024-09-23 09:29:20 +0800

    chore: 第一次提交

---
+++ assets/js/main.js
@@ -0,0 +1,182 @@
+import { Task } from "./task.js";
+
+(() => {
+  "use strict";
+  window.oncontextmenu = () => {
+    return false;
+  };
+
+  const MAX_TASK_NUM = 20;
+
+  const App = {
+    tasks: [],
+    menuItems: [
+      {
+        text: "添加任务",
+        color: "lightblue",
+        elm: null,
+      },
+      {
+        text: "删除任务",
+        color: "rgb(241, 167, 167)",
+        elm: null,
+      },
+      {
+        text: "排序方式",
+        color: "lightgreen",
+        elm: null,
+      },
+    ],
+    elms: {
+      body: null,
+      menuBox: null,
+      tasksContainer: null,
+      addTaskPanelMask: null,
+    },
+    events: {
+      addTask() {
+        if (App.methods.isListFull()) {
+          alert("列表已满，请删除部分任务后再尝试添加任务.");
+          return;
+        }
+        const addTaskPanelMask = App.elms.addTaskPanelMask;
+        addTaskPanelMask.style.display = "flex";
+        const btns = addTaskPanelMask.querySelectorAll(".__btn-area .__button");
+        const errorLen = addTaskPanelMask.querySelector(".__error-len");
+        const {
+          taskNameController,
+          taskDetailController,
+          taskDeadTimeController,
+        } = App.methods.getAddTaskPanelController(addTaskPanelMask);
+        const clearErrorLen = () => {
+          errorLen.innerHTML = "";
+        };
+        const setErrorLen = (str) => {
+          errorLen.innerHTML = str;
+        };
+        const cancelAddTask = () => {
+          addTaskPanelMask.style.display = "none";
+          App.methods.removeEvent(btns[1], "click", cancelAddTask);
+          App.methods.removeEvent(btns[0], "click", confirmAddTask);
+          App.methods.removeEvent(taskNameController, "input", clearErrorLen);
+          App.methods.removeEvent(
+            taskDeadTimeController,
+            "change",
+            clearErrorLen
+          );
+        };
+        const confirmAddTask = () => {
+          const taskName = taskNameController.value;
+          const taskDetail = taskDetailController.value.trim();
+          const deadTime = taskDeadTimeController.value;
+          if (taskName === "") {
+            setErrorLen("请填写任务名称");
+            return;
+          }
+          if (deadTime === "") {
+            setErrorLen("请选择截至日期");
+            return;
+          }
+          App.methods.addTask(new Task(taskName, taskDetail, deadTime));
+          taskNameController.value = "";
+          taskDetailController.value = "";
+          taskDeadTimeController.value = "";
+          addTaskPanelMask.style.display = "none";
+        };
+        App.methods.bindEvent(btns[1], "click", cancelAddTask);
+        App.methods.bindEvent(btns[0], "click", confirmAddTask);
+        App.methods.bindEvent(taskNameController, "input", clearErrorLen);
+        App.methods.bindEvent(taskDeadTimeController, "change", clearErrorLen);
+      },
+      delTask() {},
+      changeTaskSort() {},
+    },
+    methods: {
+      initElms(elms) {
+        console.log("start initializing elements...");
+        elms.body = document.querySelector("body");
+        elms.menuBox = document.querySelector(".menu-box");
+        elms.tasksContainer = document.querySelector(".tasks-list");
+        elms.addTaskPanelMask = document.querySelector("#add-task-panel");
+        console.log("elements loaded.");
+      },
+      renderMenuBox(menuItems) {
+        console.log("start rendering menu box...");
+        const menuBox = App.elms.menuBox;
+        const fragment = document.createDocumentFragment();
+        let i = 0;
+        for (const item of menuItems) {
+          const div = document.createElement("div");
+          div.className = "__menu-item";
+          div.innerHTML = item.text;
+          div.style.backgroundColor = item.color;
+          menuItems[i++].elm = div;
+          fragment.appendChild(div);
+        }
+        menuBox.appendChild(fragment);
+        console.log("menu box renderd successfully.");
+      },
+      bindEvent(elm, eventType, fn) {
+        if (typeof eventType !== "string" || typeof fn !== "function")
+          throw new Error(
+            `invalid parameter...eventType: ${eventType}, fn: ${fn}`
+          );
+        elm.addEventListener(eventType, fn);
+      },
+      removeEvent(elm, eventType, fn) {
+        if (typeof eventType !== "string" || typeof fn !== "function")
+          throw new Error(
+            `invalid parameter...eventType: ${eventType}, fn: ${fn}`
+          );
+        elm.removeEventListener(eventType, fn);
+      },
+      bindMenuItemEvent(menuItems, eventFns) {
+        console.log("start binding menu items events...");
+        this.bindEvent(menuItems[0].elm, "click", eventFns.addTask);
+        this.bindEvent(menuItems[1].elm, "click", eventFns.delTask);
+        this.bindEvent(menuItems[2].elm, "click", eventFns.changeTaskSort);
+        console.log("menu items events binded successfully.");
+      },
+      getScreenSize() {
+        return App.elms.body.getBoundingClientRect();
+      },
+      addTask(task) {
+        if (task instanceof Task) {
+          App.tasks.unshift(task);
+          this.renderTasks(App.elms.tasksContainer, App.tasks);
+        } else {
+          throw new Error(`invalid task type, the task is ${task}.`);
+        }
+      },
+      isListFull() {
+        return App.tasks.length >= MAX_TASK_NUM;
+      },
+      renderTasks(tasksContainer, tasks) {
+        const fragment = document.createDocumentFragment();
+        const parser = new DOMParser();
+        tasksContainer.innerHTML = "";
+        for (let i = 0; i < tasks.length; i++) {
+          const task = tasks[i];
+          fragment.append(parser.parseFromString(task.getRenderElm(i + 1), "text/html").body.childNodes[0]);
+        }
+        tasksContainer.append(fragment);
+      },
+      getAddTaskPanelController(panelElm) {
+        return {
+          taskNameController: panelElm.querySelector("#-app-task-name"),
+          taskDetailController: panelElm.querySelector("#-app-task-detail"),
+          taskDeadTimeController: panelElm.querySelector("#-app-task-date"),
+        };
+      },
+    },
+    initApp() {
+      console.log("start initializing App...");
+      this.methods.initElms(this.elms);
+      this.methods.renderMenuBox(this.menuItems);
+      this.methods.bindMenuItemEvent(this.menuItems, this.events);
+      console.log("App loaded successfully.");
+    },
+  };
+
+  App.initApp();
+})();
---
+++ assets/js/task.js
@@ -0,0 +1,30 @@
+export class Task {
+  name;
+  detail;
+  deadTime;
+
+  constructor(name, detail, deadTime) {
+    this.name = name;
+    this.detail = detail;
+    this.deadTime = deadTime;
+  }
+
+  getTaskObj() {
+    return {
+      name: this.name,
+      detail: this.detail,
+      deadTime: this.deadTime,
+    };
+  }
+
+  getRenderElm(order) {
+    return `
+      <div class="__task" title="${this.name}">
+        <div class="__order">${order}</div>
+        <p class="__title">${this.name}</p>
+        <p class="__detail">${this.detail}</p>
+        <div class="__expand" title="查看详细"></div>
+      </div>
+      `;
+  }
+}
